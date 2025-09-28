973fa73bc0cbd92392ae905dbdfa874c94ce1c41
Jörg Thalheim <joerg@thalheim.io>
2025-09-28 23:25:30 +0200

    add binary cache to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
   description = "Exploring integration between Nix and AI coding agents";
+  nixConfig = {
+    extra-substituters = [ "https://numtide.cachix.org" ];
+    extra-trusted-public-keys =
+      [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+  };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
