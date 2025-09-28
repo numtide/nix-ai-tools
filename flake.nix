18daab33af4a8d9efd1b109fa1eb24ae0df39d1e
Jörg Thalheim <joerg@thalheim.io>
2025-09-28 23:51:58 +0200

    format all code

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Exploring integration between Nix and AI coding agents";
   nixConfig = {
     extra-substituters = [ "https://numtide.cachix.org" ];
-    extra-trusted-public-keys =
-      [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+    extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
   };
 
   inputs = {
