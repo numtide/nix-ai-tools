041675760f2aba451ede0bcd2e7270c0f6574843
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-01-13 23:05:25 +0000

    remove ryoppippi from extended maintainer dict

--- lib/default.nix
+++ lib/default.nix
@@ -2,11 +2,6 @@
 inputs.nixpkgs.lib.extend (
   _final: prev: {
     maintainers = prev.maintainers // {
-      ryoppippi = {
-        github = "ryoppippi";
-        githubId = 1560508;
-        name = "Ryo Osawa";
-      };
       ypares = {
         github = "YPares";
         githubId = 1377233;
