12adf2598bb0311a4588be899af29b636f19f80b
wliao <lwlw1988@gmail.com>
2026-03-19 23:17:08 +0800

    zeroclaw: build embedded dashboard

--- packages/zeroclaw/package.nix
+++ packages/zeroclaw/package.nix
@@ -1,13 +1,17 @@
 {
   lib,
   flake,
+  stdenv,
   rustPlatform,
   fetchFromGitHub,
+  runCommand,
+  nodejs_24,
+  fetchNpmDepsWithPackuments,
+  npmConfigHook,
   versionCheckHook,
   versionCheckHomeHook,
 }:
-
-rustPlatform.buildRustPackage rec {
+let
   pname = "zeroclaw";
   version = "0.5.9";
 
@@ -18,7 +22,52 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-k/Ur6Mt8eabhsscThSSF68mExfe3oxZl2HMpqfzqgGc=";
   };
 
+  frontendSrc = runCommand "${pname}-web-src-${version}" { } ''
+    mkdir -p $out
+    cp -r ${src}/web/. $out/
+  '';
+
+  frontend = stdenv.mkDerivation {
+    pname = "${pname}-frontend";
+    inherit version;
+    src = frontendSrc;
+
+    nativeBuildInputs = [
+      nodejs_24
+      npmConfigHook
+    ];
+
+    npmDeps = fetchNpmDepsWithPackuments {
+      src = frontendSrc;
+      name = "${pname}-${version}-npm-deps";
+      hash = "sha256-FUbGO4fNBtusaOcdrtnWdRHSiK8bjxOTAbgC5A0GDno=";
+      fetcherVersion = 2;
+    };
+    makeCacheWritable = true;
+
+    buildPhase = ''
+      runHook preBuild
+      npm run build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r dist/* $out/
+      runHook postInstall
+    '';
+  };
+in
+rustPlatform.buildRustPackage rec {
+  inherit pname version src;
+
   cargoHash = "sha256-cdlD2GEOZTuOA/JeDGFokTVRZ2aozpuIGJwlDiiPNmE=";
+
+  preBuild = ''
+    mkdir -p web/dist
+    cp -r ${frontend}/* web/dist/
+  '';
 
   # Tests require runtime configuration and network access
   doCheck = false;
@@ -34,7 +83,7 @@ rustPlatform.buildRustPackage rec {
   meta = {
     description = "Fast, small, and fully autonomous AI assistant infrastructure";
     homepage = "https://github.com/zeroclaw-labs/zeroclaw";
-    changelog = "https://github.com/zeroclaw-labs/zeroclaw/releases";
+    changelog = "https://github.com/zeroclaw-labs/zeroclaw/releases/tag/v${version}";
     license = lib.licenses.mit;
     sourceProvenance = with lib.sourceTypes; [ fromSource ];
     maintainers = with flake.lib.maintainers; [ commandodev ];
